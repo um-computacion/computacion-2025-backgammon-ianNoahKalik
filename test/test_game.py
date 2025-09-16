8c8f4007c414af7deb3e59307fcebf7c4fa38585
unknown <i.kalik@alumno.um.edu.ar>
2025-09-16 10:25:03 -0300

    agrego test faltantes a la clase game

--- test/test_game.py
+++ test/test_game.py
@@ -40,5 +40,34 @@ class TestJuegoBackgammon(unittest.TestCase):
         self.juego.tablero.fuera[self.juego.jugador_blanco.color] = 15
         self.juego.simular_partida()  # Solo debería imprimir el ganador
 
+    def test_obtener_ganador_blanco(self):
+        self.juego.tablero.fuera[self.juego.jugador_blanco.color] = 15
+        ganador = self.juego.obtener_ganador()
+        self.assertEqual(ganador, self.juego.jugador_blanco)
+
+    def test_obtener_ganador_negro(self):
+        self.juego.tablero.fuera[self.juego.jugador_negro.color] = 15
+        ganador = self.juego.obtener_ganador()
+        self.assertEqual(ganador, self.juego.jugador_negro)
+
+    def test_obtener_ganador_none(self):
+        ganador = self.juego.obtener_ganador()
+        self.assertIsNone(ganador)
+
+    def test_reiniciar_partida(self):
+        self.juego.tablero.fuera[self.juego.jugador_blanco.color] = 15
+        self.juego.jugador_blanco.fichas_capturadas = 5
+        self.juego.jugador_blanco.fichas_salidas = 10
+        self.juego.jugador_blanco.fichas_en_tablero = 0
+        self.juego.turno_actual = self.juego.jugador_negro
+
+        self.juego.reiniciar_partida()
+
+        self.assertEqual(self.juego.jugador_blanco.fichas_capturadas, 0)
+        self.assertEqual(self.juego.jugador_blanco.fichas_salidas, 0)
+        self.assertEqual(self.juego.jugador_blanco.fichas_en_tablero, 15)
+        self.assertEqual(self.juego.turno_actual, self.juego.jugador_blanco)
+        self.assertEqual(self.juego.tablero.obtener_fuera(self.juego.jugador_blanco.color), 0)        
+
 if __name__ == "__main__":
     unittest.main()
